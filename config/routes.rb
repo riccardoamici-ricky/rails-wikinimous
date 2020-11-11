bf38b9d1635c0caa85ea881897be3abcce9a3dcd
riccardo amici <amici95@gmail.com>
2020-11-11 10:43:47 +0100

    sec ex

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'articles/:id/edit', to: 'articles#edit'
+  get 'articles/new', to: 'articles#new'
+  get 'articles', to: 'articles#index', as: :home
+  get 'articles/:id', to: 'articles#show', as: :article
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy', as: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  new_article = Article.create(title: Faker::Games::Pokemon.name, content: Faker::TvShows::Simpsons.quote)
+end
